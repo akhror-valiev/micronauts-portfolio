022705f4b811a153bcb9bcb154cd509ed59aca71
akhror-valiev <ahror.valiev@gmail.com>
2022-10-12 11:44:42 +0500

    Add pop up and js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-menu');
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active');
-  navMenu.classList.toggle('active');
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav-link').forEach((navLink) => navLink.addEventListener('click', () => {
-  hamburgerMenu.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
+document.querySelectorAll(".nav-link").forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+const previewContainer = document.querySelector(".preview-section");
+const previewBox = previewContainer.querySelectorAll(".preview-container");
+
+document
+  .querySelectorAll(".project-section .project-items")
+  .forEach((project) => {
+    project.onclick = () => {
+      previewContainer.style.display = "flex";
+      let name = project.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        let target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active");
+        }
+      });
+    };
+  });
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-close").onclick = () => {
+    close.classList.remove("active");
+    previewContainer.style.display = "none";
+  };
+});
