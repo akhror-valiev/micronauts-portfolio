bf56e8f4a6d8a553504549fbd9bcb57a00cf5f27
akhror-valiev <ahror.valiev@gmail.com>
2022-10-13 14:50:31 +0500

    Fix linter error

--- js/script.js
+++ js/script.js
@@ -1,98 +1,96 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const navMenu = document.querySelector(".nav-menu");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach((navLink) =>
-  navLink.addEventListener("click", () => {
-    hamburgerMenu.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((navLink) => navLink.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
 
 const projectData = [
   {
-    id: "p-1",
+    id: 'p-1',
     img: {
-      src: "./images/Snapshoot Portfolio1.svg",
-      alt: "tonic project preview",
+      src: './images/Snapshoot Portfolio1.svg',
+      alt: 'tonic project preview',
     },
-    title: "Tonic",
-    type: ["CANOPY", "Back End", "2015"],
+    title: 'Tonic',
+    type: ['CANOPY', 'Back End', '2015'],
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: [
-      "HTML",
-      "CSS",
-      "Ruby and Rails",
-      "Javascript",
-      "github",
-      "bootstrap",
+      'HTML',
+      'CSS',
+      'Ruby and Rails',
+      'Javascript',
+      'github',
+      'bootstrap',
     ],
   },
   {
-    id: "p-2",
+    id: 'p-2',
     img: {
-      src: "./images/Snapshoot Portfolio2.svg",
-      alt: "Multi-Post Stories project preview",
+      src: './images/Snapshoot Portfolio2.svg',
+      alt: 'Multi-Post Stories project preview',
     },
-    title: "Multi-Post Stories",
-    type: ["FACEBOOK", "Full Stack Dev", "2015"],
+    title: 'Multi-Post Stories',
+    type: ['FACEBOOK', 'Full Stack Dev', '2015'],
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     technologies: [
-      "HTML",
-      "Ruby on Rails",
-      "CSS",
-      "Javascript",
-      "github",
-      "bootstrap",
+      'HTML',
+      'Ruby on Rails',
+      'CSS',
+      'Javascript',
+      'github',
+      'bootstrap',
     ],
   },
   {
-    id: "p-3",
+    id: 'p-3',
     img: {
-      src: "./images/Snapshoot Portfolio3.svg",
-      alt: "Facebook 360 project preview",
+      src: './images/Snapshoot Portfolio3.svg',
+      alt: 'Facebook 360 project preview',
     },
-    title: "Facebook 360",
-    type: ["CANOPY", "BACKEND DEV", "2015"],
+    title: 'Facebook 360',
+    type: ['CANOPY', 'BACKEND DEV', '2015'],
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     technologies: [
-      "HTML",
-      "Ruby on Rails",
-      "CSS",
-      "Javascript",
-      "github",
-      "bootstrap",
+      'HTML',
+      'Ruby on Rails',
+      'CSS',
+      'Javascript',
+      'github',
+      'bootstrap',
     ],
   },
   {
-    id: "p-4",
+    id: 'p-4',
     img: {
-      src: "./images/Snapshoot Portfolio4.svg",
-      alt: "Uber Navigation project preview",
+      src: './images/Snapshoot Portfolio4.svg',
+      alt: 'Uber Navigation project preview',
     },
-    title: "Uber Navigation",
-    type: ["UBER", "Lead Developer", "2018"],
+    title: 'Uber Navigation',
+    type: ['UBER', 'Lead Developer', '2018'],
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     technologies: [
-      "HTML",
-      "Ruby on Rails",
-      "CSS",
-      "Javascript",
-      "github",
-      "bootstrap",
+      'HTML',
+      'Ruby on Rails',
+      'CSS',
+      'Javascript',
+      'github',
+      'bootstrap',
     ],
   },
 ];
 
-const projectElement = document.querySelector(".preview-section");
+const projectElement = document.querySelector('.preview-section');
 
 function renderProjects() {
   projectData.forEach((data) => {
@@ -141,32 +139,30 @@ function renderProjects() {
 
 renderProjects();
 
-let previewContainer = document.querySelector(".preview-section");
-let previewBox = previewContainer.querySelectorAll(".preview-container");
+const previewContainer = document.querySelector('.preview-section');
+const previewBox = previewContainer.querySelectorAll('.preview-container');
 
 document
-  .querySelectorAll(".project-section .project-items")
+  .querySelectorAll('.project-section .project-items')
   .forEach((project) => {
     project.onclick = () => {
-      previewContainer.style.display = "flex";
+      previewContainer.style.display = 'flex';
 
-      let name = project.getAttribute("data-name");
-      console.log(previewBox);
+      const name = project.getAttribute('data-name');
 
       previewBox.forEach((preview) => {
-        let target = preview.getAttribute("data-target");
+        const target = preview.getAttribute('data-target');
 
-        if (name == target) {
-          preview.classList.add("active");
-
+        if (name === target) {
+          preview.classList.add('active');
         }
       });
     };
   });
 
 previewBox.forEach((close) => {
-  close.querySelector(".fa-close").onclick = () => {
-    close.classList.remove("active");
-    preveiwContainer.style.display = "none";
+  close.querySelector('.fa-close').onclick = () => {
+    close.classList.remove('active');
+    previewContainer.style.display = 'none';
   };
 });
